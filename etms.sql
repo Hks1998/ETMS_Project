affa4f1d4de97a7d974eb663d1e210cd9152f344
D2_Darshan_56567 <darshanrgaikwad1997@gmail.com>
2022-01-21 17:53:09 +0530

    project table created

--- etms.sql
+++ etms.sql
@@ -12,3 +12,15 @@ assignment_status  varchar(100));
 alter table employee auto_increment=101;
 
 insert into employee (name,email,password,role,department,designation,mgr_id ,assignment_status) values('hks','hks@test.com','123456','manager','1','CA',102,'complete'); 
+
+
+CREATE TABLE project (project_id INT primary key auto_increment ,
+project_name VARCHAR(100) ,
+ status VARCHAR(50) , 
+ project_description VARCHAR(300),
+ start_date date ,
+ due_date date );
+ 
+ alter table project auto_increment 1;
+ 
+ 
