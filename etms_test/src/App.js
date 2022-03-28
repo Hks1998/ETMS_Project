2b6eb2b784d67a6a618bd407453a336322a0ca15
Kapil Upadhyay <upadhyaykapil31@gmail.com>
2022-03-28 13:39:38 +0530

    Signin, ManagerHome, taskAssign pages created

--- etms_test/src/App.js
+++ etms_test/src/App.js
@@ -1,6 +1,8 @@
 import './App.css'
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
+import ManagerHome from "./pages/ManagerHome"
+import TaskAssign from "./pages/TaskAssign"
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 
 function App() {
@@ -8,10 +10,13 @@ function App() {
     <div className="container">
       
       <h1>Employee Task Management System</h1>
+      <hr />
       <BrowserRouter>
         <Routes>
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/managerHome" element={<ManagerHome />} />
+          <Route path="/taskAssign" element={<TaskAssign />} />
         </Routes>
       </BrowserRouter>
     </div>
