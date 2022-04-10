b71dfc6b0f4aee5961ff13b268d4d8ec6cd8f0e9
D2_56439_Harsh <harshkumarbvm@gmail.com>
2022-04-10 14:02:39 +0530

    Admin_Home page done

--- etms_test/src/pages/Admin_Home/index.js
+++ etms_test/src/pages/Admin_Home/index.js
@@ -1,6 +1,28 @@
-import { useNavigate } from 'react-router'
+import { useLocation,useNavigate } from 'react-router'
+import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+import Employee from '../../components/Employee'
+import { URL } from '../../config'
 
+import axios from 'axios'
 const Admin_Home = () => {
+  const [employee, setEmployees] = useState([])
+  const id = sessionStorage['empId']
+  const Name = sessionStorage['Name']
+
+  const searchEmployees = () => {
+      const url = `${URL}/admin/list_employees`
+      axios.get(url).then((response) => {
+          const result = response.data
+          if (result['status'] == 'success') {
+              setEmployees(result['data'])
+          } else {
+              toast.error(result['error'])
+          }
+      })
+  }
+
+
 
   const navigate = useNavigate()
 
@@ -8,11 +30,26 @@ const Admin_Home = () => {
     // remove the logged users details from session storage
     sessionStorage.removeItem('empId')
     sessionStorage.removeItem('name')
-    sessionStorage.removeItem('dept.deptId')
+    sessionStorage.removeItem('deptId')
     sessionStorage.removeItem('loginStatus')
 
     navigate('/signin')
   }
+  const addEmployee = () => {
+    
+    navigate('/addemployee')
+
+  }
+  const employeeProfile = () => {
+    navigate('/employee_profile')
+  }
+  const changePassword = () => {
+    navigate('/changepassword')
+  }
+  useEffect(() => {
+    searchEmployees()
+}, [])
+ 
 
   return (
     <>
@@ -30,11 +67,13 @@ const Admin_Home = () => {
 
           <div class="btn-group">
             <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-              Welcome,Username
+              Welcome,{Name}
   </button>
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Profile</a></li>
-              <li><a class="dropdown-item" href="#">Change Password</a></li>
+              <li><a class="dropdown-item" href="#"><button onClick={employeeProfile} className="dropdown-item">
+                  My Profile</button></a></li>
+              <li><a class="dropdown-item" href="#"><button onClick={changePassword} className="dropdown-item">
+                  Change Password</button></a></li>
               <li><hr class="dropdown-divider" /></li>
               <li><a class="dropdown-item" href="#">
                 <button onClick={logoutUser} className="dropdown-item">
@@ -52,7 +91,7 @@ const Admin_Home = () => {
         <br />
         <div className="row">
           <div className="col"></div>
-          <div className="col"><button type="button" class="btn btn-primary"><h3>Add New Employee</h3></button></div>
+          <div className="col"><button onClick={addEmployee} type="button" class="btn btn-primary"><h3>Add New Employee</h3></button></div>
           <div className="col"></div>
         </div>
       </div>
@@ -66,14 +105,15 @@ const Admin_Home = () => {
               <th scope="col">ID</th>
               <th scope="col">Name</th>
               <th scope="col">Email</th>
-              <th scope="col">Mobile</th>
+              <th scope="col">Phone</th>
+              <th scope="col">Password</th>
               <th scope="col"> Department ID</th>
               <th scope="col"> Manager ID</th>
               <th scope="col"> Action</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
+            {/* <tr>
               <th scope="row">101</th>
               <td>Harsh</td>
               <td>harsh@test.com</td>
@@ -100,7 +140,10 @@ const Admin_Home = () => {
                 <div className="col">
                   <button type="button" class="btn btn-primary">Delete</button>
                 </div></div></td>
-            </tr>
+            </tr> */}
+            {employee.map((tempEmployee) => {
+                                return <Employee employee = {tempEmployee} />
+                            })}
 
 
           </tbody>
@@ -110,4 +153,4 @@ const Admin_Home = () => {
   );
 };
 
-export default Admin_Home;
+export default Admin_Home
